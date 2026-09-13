6f7c8269b7fd659b0d88b1efb72dcd8436b40852
Juansolarte-3309 <juanpablosolartealvarez189@gmail.com>
2026-09-12 20:08:54 -0500

    Version final-integradora

--- ZooCare.java
+++ ZooCare.java
@@ -1,16 +1,1 @@
 import java.util.Scanner;
-/**
- * Analisis:
- * 
- * 
- * 
- * 
- */
-
-public class ZooCare{
-    public static Scanner sc = new Scanner(System.in);
-
-    public static void main(String args[]){
-
-    }
-}
