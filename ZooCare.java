a9ed7663e572adde8b77cb6b76139e21c38caff5
Juansolarte-3309 <juanpablosolartealvarez189@gmail.com>
2026-09-12 19:59:06 -0500

    integradora con todos los requerimientos

--- ZooCare.java
+++ ZooCare.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
-
+/**
+ * Analisis:
+ * 
+ * 
+ * 
+ * 
+ */
 
 public class ZooCare{
-
+    public static Scanner sc = new Scanner(System.in);
 
     public static void main(String args[]){
 
